a2feb974ac9cc51c21721d9253ec80274ab4b147
Krhyst Ferrari <krhys_ferrari@hotmail.com>
2022-10-30 20:24:59 -0300

    Aula108

---
+++ context-api/src/context/CounterContext.js
@@ -0,0 +1,12 @@
+// 1 - criando o contexto
+import { createContext, useState } from "react"
+
+const CounterContext = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CounterContext = createContext()
