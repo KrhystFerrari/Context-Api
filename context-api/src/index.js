403a20fe1ee316c7a74ca628083877264db84e9e
Krhyst Ferrari <krhys_ferrari@hotmail.com>
2022-10-30 22:10:12 -0300

    update

--- context-api/src/index.js
+++ context-api/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
 import { CounterContextProvider } from "./context/CounterContext";
 import { TitleColorContextProvider } from "./context/TitleColorContext";
 
